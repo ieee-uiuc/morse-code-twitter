77d29a58d6b8bae8e0c5199b63bc09bc15dd3b50
Kashev Dalmia <kashev.dalmia@gmail.com>
2016-02-17 00:19:36 +0000

    morse code reading mostly works

--- scratch/button_test.py
+++ scratch/button_test.py
@@ -6,11 +6,10 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     button_pressed = not GPIO.input(18)
     if button_pressed:
         print('Button Pressed')
-        time.sleep(0.2)
+        time.sleep(0.02)
--- scratch/morse_button.py
+++ scratch/morse_button.py
@@ -6,36 +6,78 @@ import RPi.GPIO as GPIO
 import time
 
 
+# Constants
+MORSE_PIN = 18
+DOT = 100  # ms
+DASH = 3 * DOT
+
+
 def millis():
     """ Function which mimics Arduino millis() """
     return int(round(time.time() * 1000))
 
 
-MORSE_PIN = 18
+def setup():
+    """ Setup function. Sets up the GPIO on the Pi. """
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(MORSE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def loop():
     """ Loop will run and is not expected to exit. """
     prev_button_pressed = False
     time_var = 0
+    curr_tweet = ""
+    tweet_started = False
 
     while True:
         curr_button_pressed = not GPIO.input(MORSE_PIN)
-
-        if not curr_button_pressed:
-            if prev_button_pressed:
+        
+        if curr_button_pressed:
+            if not prev_button_pressed:  # Button was just pressed
+                # How long was it up?
                 duration = millis() - time_var
-                print(duration)
-                if duration < 20:
-                    continue
+
+                if DASH < duration <= 1.5 * DASH:
+                    curr_tweet += ' '
+                elif 1.5 * DASH < duration < 3 * DASH:
+                    curr_tweet += '  '
+                
+                # Keep Track of how long it will be pressed.
+                time_var = millis()
+                prev_button_pressed = True
+                tweet_started = True
+
+                # How long was it up?
+
+        else:  # Button not pressed
+            if prev_button_pressed:  # Button was just released
+                duration = millis() - time_var
+                time_var = millis()
+                
+                # Determine if the duration held was a dot or a dash.
+                if duration <= 1.5 * DOT:
+                    curr_tweet += '.'
+                else:
+                    curr_tweet += '-'
 
                 prev_button_pressed = False
-        else:
-            prev_button_pressed = True
+            else:  # Button has been up. How long?
+                duration = millis() - time_var
+                
+                if duration >= 3 * DASH and tweet_started:
+                    print(curr_tweet)
+                    curr_tweet = ""
+                    tweet_started = False
+
+        
+        # Debouncing Kludge
+        time.sleep(0.01)
 
 
 def main():
     """ Main entry point. """
+    setup()
     loop()
 
 
