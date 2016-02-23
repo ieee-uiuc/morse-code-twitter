2264410fad4c29ab91a7963728aa4d6f39b49fce
Kashev Dalmia <kashev.dalmia@gmail.com>
2016-02-23 21:59:42 +0000

    Finished tweeting part, MVP done

---
+++ src/example_secrets.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+AuthKeys = {
+    'token':           'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'token_secret':    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'consumer_key':    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'consumer_secret': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+}
--- src/tweeting.py
+++ src/tweeting.py
@@ -2,8 +2,31 @@
 # -*- coding: utf-8 -*-
 # morse-code-twitter
 
+import twitter
+from secrets import AuthKeys
+
+t = twitter.Twitter(auth=twitter.OAuth(
+    AuthKeys['token'], AuthKeys['token_secret'],
+    AuthKeys['consumer_key'], AuthKeys['consumer_secret']))
+
 
 def send_tweet(tweet):
     """ Given a tweet, tweet it. """
-    # STUB
     print(tweet)
+    t.statuses.update(status=tweet)
+
+
+def main():
+    """ Tweet an example tweet. """
+    import sys
+
+    if len(sys.argv) == 1:
+        print('USAGE: ./tweeting.py YOUR TWEET')
+        return
+
+    tweet = ' '.join(sys.argv[1:])
+    send_tweet(tweet)
+
+
+if __name__ == '__main__':
+    main()
